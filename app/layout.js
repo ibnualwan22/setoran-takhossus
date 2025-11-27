6e3b075078a2eb220935188d4d0762425822f349
ibnualwan22 <allwanecahzobo@gmail.com>
2025-11-27 15:45:18 +0700

    add PWA

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,16 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Setoran Takhossus',
   description: 'Aplikasi Setoran Harian Asrama Takhossus',
+  manifest: '/manifest.json', // Link otomatis ke manifest yang digenerate Next.js
+};
+
+// TAMBAHKAN INI:
+export const viewport = {
+  themeColor: "#4f46e5", // Warna bar notifikasi di HP Android
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false, // Mencegah zoom cubit (opsional, biar rasa native app)
 };
 
 export default function RootLayout({ children }) {
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public", // Tujuan folder service worker
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development", // Matikan PWA saat mode dev agar tidak mengganggu
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
